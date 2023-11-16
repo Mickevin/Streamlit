dd9fa88a854c7f6b5b7562c1db13bf3c508c2029
Kévin Duranty <70485959+Quera-fr@users.noreply.github.com>
2023-11-16 10:56:19 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# Import de la bibliothèque streamlit
+import streamlit as st
+
+# Titre de la page
+st.title("Mon premier titre")
+
+# Input de texte
+user_input = st.text_input("Entrez votre texte")
+
+# Affichage du texte
+st.write("Vous avez entré : ", user_input)
+
+# Bouton
+bouton = st.button("Cliquez ici")
+
+if bouton:
+    st.write("Vous avez cliqué sur le bouton")
+    # Affichage d'une image
+    st.image('img.jpg')
+
+# Formulaire streamlit avec champs Nom, prénom age et bouton
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+       st.write("Outside the form")
